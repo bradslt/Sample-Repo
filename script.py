7c83dbf6d66685fb0794ed10adc75c40f9f99276
Brad MacKeil <brad@springloadedtech.com>
2020-02-07 11:29:59 -0400

    Removed some code

--- script.py
+++ script.py
@@ -1,18 +1,6 @@
-import math
-import sys
-from os import rename
-
 import requests
-
-print(sys.version)
-print(sys.executable)
-
-
-def greet(whoToGreet):
-    greeting = "Hello, {}".format(whoToGreet)
-    return greeting
-
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
 
